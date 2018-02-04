e47c3669335104d7ddf6bc50ef13a42d677ae7b8
w0d <35996314+w0d@users.noreply.github.com>
2018-02-04 23:53:37 +0000

    simple regular expression handling

--- symbolLiterals.js
+++ symbolLiterals.js
@@ -1,54 +1,47 @@
 // transpile javascript containing symbol literals, #mySymbolName, to valid javascript => Symbol.for('mySymbolName')
 // USE: main(scriptText); returns valid javascript
-//  
+// Caution advised if you use regex literals..
 
 var stream;
 var streamPos;
 var result = "";
 var look;
 var EOF = false;
-
 var CR = "\r";
 var LF = "\n";
 var QUOTES = "\'"+'\"';
 var STRINGESCAPE = "\\";
+var EOFMaxRead = 1000;
 
 function read(){
   if (streamPos < stream.length) {
     look = stream[streamPos];
     streamPos = streamPos + 1;
+    if (streamPos >= stream.length){
+      EOF = true;
+    }
   } else {
-    EOF = true;
     look = "";
-  }
-  if (streamPos == 146493){
-    look = look;
+    if (!EOFMaxRead--) abort("ABORTED: Stuck in loop..");
   }
 }
-
 function getChar(){
   read();
 }
-
 function error(s){
-  console.log("Error: " + s + ".");
+  console.log("Error@char " + (streamPos-1)+ " : " + s  );
 }
-
 function abort(s){ 
   error(s);
-  console.log("ERROR at char "+(streamPos-1));
   throw new Error(s);
 }
-
 function expected(s){
   abort(s + " Expected");
 }
-
 function match(c){
   if (look == c) getChar();
   else expected("'" + c + "'");
 }
-
 function matchOne(c){
   for (var i=0; i<c.length; i++){
     if (look == c[i]) {
@@ -58,136 +51,121 @@ function matchOne(c){
   }
   expected("'One of " + c + "'");
 }
-
 function isAlpha(c){
   var charCode = c.charCodeAt(0);
   return (charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123);
 }
-
 function isDigit(c){
   var charCode = c.charCodeAt(0);
   return (charCode > 47 && charCode < 58);
 }
-
 function isAlNum(c) {
   return isAlpha(c) || isDigit(c);
 }
-
 function isQuote(c){
   return "'" == c || '"' == c;
 }
-
 function isHash(c){
   return c == "#";
 }
-
 function isSingleLineComment(c){ //cheeky lookahead
   return (c == "/") && ("/" == stream[streamPos]);
 }
-
 function isMultiLineComment(c){ //cheeky lookahead
   return (c == "/") && ("*" == stream[streamPos]);
 }
-
-function isEndOfLine(c){
-  return (c == CR) || (c == LF);
+function isEndOfLineOrFile(c){
+  return (c == CR) || (c == LF) || EOF;
 }
-
-function getRegularExpressionLiteral(){
-
+function isRegExLiteral(c){ //**NB: just catches the breaking expressions /\/\//g; & /a\/*abc()/gi
+  var isRegExpTell = (streamPos>1) && (STRINGESCAPE == stream[streamPos-2]);  //(streamPos>1) for when script begins with // or /*
+  var isRegExpDoubleSlash = isSingleLineComment(c) && isRegExpTell;
+  var isRegExpSlashStar = isMultiLineComment(c) && isRegExpTell;
+  return isRegExpDoubleSlash || isRegExpSlashStar;
 }
-
-function getSymbolLiteral(){
-  var ret = '';
-  match('#');
-  if (!isAlpha(look)) expected('Symbol literal name');
-  while (isAlNum(look)) {
-    ret += look;
-    getChar();
-  }
-  return ret;
-}
-
-function getStringLiteral(){
-  var ret = matchOne(QUOTES);
-  while (look != ret[0] && !EOF && !isEndOfLine(look)) {    //EOF check required for scripts with a lonely quote
-    ret += look;
-    if (look == STRINGESCAPE) {   //skip next char as well
-      getChar();
-      ret += look;
-    }
-    getChar();
-  }
-  if (isEndOfLine(look)) console.log("String Literal Error: End of line before string terminator")
-  ret += look;
-  getChar();
-  return ret;
-}
-
-function getMultiLineComment(){
-  var ret = '';
-  do {
-    do {
-      ret += look;
-      getChar();
-    } while (look != '*' && !EOF);    //EOF check required for scripts with unterminated comment
-    ret += look;
-    getChar();
-  } while (look != '/' && !EOF);
-  ret += look;
-  getChar();
-  return ret;
-}
-
-function getSingleLineComment(){
-  var ret = '';
-  while (!isEndOfLine(look) && !EOF) {    //EOF check required for scripts with comment on last line
-    ret += look;
-    getChar();
-  }
-  ret += look;
-  getChar();
-  return ret;
-}
-
 function emit(s){
   result += s;
 }
-
+function emitAndGetChar(s){
+  emit(s);
+  getChar();
+}
+function emitRegExLiteral(c){
+//do while not / where / is not preceeded by \ or \\\ or \\\\\ etc          /\//g; or  /a\/*/g;
+  if (isSingleLineComment(c) && (streamPos>1) && (STRINGESCAPE == stream[streamPos-2])){
+    emitAndGetChar(look);
+  } else if (isMultiLineComment(c) && (streamPos>1) && (STRINGESCAPE == stream[streamPos-2])) {
+    while ((look != "/" && STRINGESCAPE != stream[streamPos-1]) || !EOF) {
+      emitAndGetChar(look);
+    }
+  } else {
+    console.log('u aint seen me..');
+  }
+  emitAndGetChar(look);
+}
+function emitSymbolLiteral(){
+  match('#');
+  emit( "Symbol.for('" );
+  if (!isAlpha(look)) expected('Symbol literal name');
+  while (isAlNum(look)) {
+    emitAndGetChar( look );
+  }
+  emit("')");
+}
+function emitStringLiteral(){
+  var thisStringsTerminator = matchOne(QUOTES);
+  emit(thisStringsTerminator);
+  while (look != thisStringsTerminator && !isEndOfLineOrFile(look)) {
+    if (look == STRINGESCAPE) {   //skip next char as well
+      emitAndGetChar(look);
+    }
+    emitAndGetChar(look);
+  }
+  if (look != thisStringsTerminator) error("String Literal Error: End of line or file: expecting string terminator");
+  emitAndGetChar(look);
+}
+function emitMultiLineComment(){
+  while ((look != "/" && "*" != stream[streamPos-1]) || EOF) {
+    emitAndGetChar(look);
+  }
+  emitAndGetChar(look);
+}
+function emitSingleLineComment(){
+  while (!isEndOfLineOrFile(look)) {
+    emitAndGetChar(look);
+  }
+  emitAndGetChar(look);
+}
 function init(){
   result = "";
   EOF = false;
   getChar();
 }
-
 function replaceSymbolLiterals(){
-  if (look=="#"){
-    look == look;
-  }
   switch (true) {
+    case isRegExLiteral(look):
+      emitRegExLiteral();
+      break;
     case isSingleLineComment(look):
-console.log("single comment");
-      emit( getSingleLineComment() );
+      emitSingleLineComment();
       break;
     case isMultiLineComment(look):
-console.log("multi comment");
-      emit( getMultiLineComment() );
+      emitMultiLineComment();
       break;
     case isQuote(look):
-      emit( getStringLiteral() );
+      emitStringLiteral();
       break;
     case isHash(look):
-      emit( "Symbol.for('"+getSymbolLiteral()+"')" );
+      emitSymbolLiteral();
       break;
     default:
-      emit( look );
-      getChar();
+      emitAndGetChar(look);
   }
 }
-
 function main(parseStr){
   stream = parseStr;
   streamPos = 0;
+  EOFMaxRead = 1000;
   init();
   while (!EOF){
     replaceSymbolLiterals();
